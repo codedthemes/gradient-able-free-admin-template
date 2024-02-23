42a7edb96517a7051020edbaf0c98c1687dfabf6
CodedThemes <77041536+phoenixcoded20@users.noreply.github.com>
2024-02-23 11:32:47 +0530

    Update constant.js

--- react/src/config/constant.js
+++ react/src/config/constant.js
@@ -1,4 +1,4 @@
-export const BASENAME = '/demos/admin-templates/gradient-able/react/default';
+export const BASENAME = '/demos/admin-templates/gradient-able/react/free';
 export const BASE_URL = '/app/dashboard/analytics';
 export const BASE_TITLE = ' | Gradient Able Free React Admin Template';
 
