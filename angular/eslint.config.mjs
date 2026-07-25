82094551aa7ed0ea044c6671b0bcb7724c900991
ct-anjali-patel <anjali.rana@phoenixcoded.co>
2026-07-25 11:51:19 +0530

    free version angular testing done

--- angular/eslint.config.mjs
+++ angular/eslint.config.mjs
@@ -1,9 +1,6 @@
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
 import angular from 'angular-eslint';
-import { createRequire } from 'module';
-
-const require = createRequire(import.meta.url);
 
 export default tseslint.config(
     {
